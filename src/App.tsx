6ab85ab520af5cd4a6b0a44690ce75a4f1dbe659
ZclipseCode <bmichaelbusiness@gmail.com>
2026-03-01 14:24:44 -0600

    window width

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ export default function App() {
       <Window
         title="Hello World"
         icon={smiley}
-        width={600}
+        width={"40%"}
       >
         <p>hi you're probably looking for <a href="https://zclipsedev.com/">zclipsedev.com</a></p>
       </Window>
